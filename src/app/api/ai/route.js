bd7d0e2fb553e5d4ce5b7543bb3ddfe81f679015
mabgcm <bugucam@gmail.com>
2024-02-26 13:33:00 -0500

    asd

--- src/app/api/ai/route.js
+++ src/app/api/ai/route.js
@@ -14,7 +14,7 @@ export async function POST(request) {
         messages: [
             {
                 role: "system",
-                content: "You're an education specialist. Create a learning objective content in a single sentence for the user  according to their input. The content should be in a h1 element."
+                content: "You're an education specialist. Answer the user"
             },
             {
                 role: "user",
